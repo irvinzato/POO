302b2befcbaa28515df611d4937c259d3ded8b30
irvinzato <multizato@hotmail.com>
2022-08-05 19:35:21 -0500

    Objetos, encapsulamiento, polimorfismo, principio de ocultar variables, get, set

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,19 +1,75 @@
 public class Automovil {
-  //La idea es que maneje datos la clase y los retorne
-  String maker;
-  String model;
-  String color = "Blanco";
-  double displacement;
+  //La idea es que maneje datos la clase y los retorne... this hace referencia a los atributos de la clase
+  private String maker;
+  private String model;
+  private String color = "Blanco";
+  private double displacement;
+  private int tankCapacity = 40;
+
+  //Métodos GET para obtener valores privados
+  public String getMaker() {
+    return this.maker;
+  }
+  public String getModel() {
+    return this.model;
+  }
+  public String getColor() {
+    return this.color;
+  }
+  public double getDisplacement() {
+    return this.displacement;
+  }
+  public int getTankCapacity() {
+    return this.tankCapacity;
+  }
+
+  //Métodos SET para actualizar valores
+  public void setMaker( String maker ) {
+    this.maker = maker;
+  }
+  public void setModel( String model ) {
+    this.model = model;
+  }
+  public void setColor( String color ) {
+    this.color = color;
+  }
+  public void setDisplacement( double displacement ) {
+    this.displacement = displacement;
+  }
+  public void setTankCapacity( int tankCapacity ) {
+    this.tankCapacity = tankCapacity;
+  }
 
   public String detail() {
     //Con "this" hace referencia a sí mismo dentro de la clase(avaces se puede omitir, depende la situación)
     String maker = "Por definir";
-    StringBuilder sb = new StringBuilder();
-    sb.append("maker = " + this.maker);
-    sb.append("\nmodel = " + this.model);
-    sb.append("\ncolor = " + this.color);
-    sb.append("\ndisplacement = " + this.displacement);
-    return sb.toString();
+    return  "maker = " + this.maker +
+            "\nmodel = " + this.model +
+            "\ncolor = " + this.color +
+            "\ndisplacement = " + this.displacement;
+  }
+
+  public String speedUp( int rpm ) {
+    return "El auto " + this.maker + " acelerando a " + rpm + " rpm";
+  }
+
+  public String stop() {
+    return this.maker + " " + this.model + " frenando";
+  }
+
+  public String speedUpStop( int rpm ) {
+    String speedUp = this.speedUp( rpm );
+    String stop = this.stop();
+    return speedUp + "\n" + stop;
+  }
+
+  //Esto es SOBRE CARGA de métodos. Mismo nombre de método pero diferentes parámetros que recibe
+  public float calculateConsumption( int km, float percentage ) {
+    return km/(tankCapacity*percentage);
+  }
+
+  public float calculateConsumption( int km, int percentage ) {
+    return km/(tankCapacity*(percentage/100f));
   }
 
 }
--- src/AutomovilEjemplo.java
+++ src/AutomovilEjemplo.java
@@ -3,18 +3,31 @@ public class AutomovilEjemplo {
     Automovil car = new Automovil();
     Automovil car2 = new Automovil();
 
-    car.maker = "Subaru";
-    car.model = "I 10";
-    car.displacement = 2.0;
-    car.color = "Negro";
-    
-    car2.maker = "Mazda";
-    car2.color = "Azul";
-    car2.displacement = 3.0;
+    //Principio de ocultar variables de nuestra clase
+    car.setMaker("Subaru");
+    car.setModel("I-10");
+    car.setDisplacement(2.5);
+    car.setColor("Negro");
+
+    car2.setMaker("Mazda");
+    car2.setModel("BT-50");
+    car2.setDisplacement(3.5);
+    car2.setColor("Azul");
 
     System.out.println("Detalles del primer carro");
     System.out.println( car.detail() );
     System.out.println("Detalles del segundo carro");
     System.out.println( car2.detail() );
+
+    System.out.println(car.speedUp( 3000 ));
+    System.out.println(car.stop());
+    System.out.println("Combinacion de metodos");
+    System.out.println(car.speedUpStop( 4000 ));
+
+    //ENCAPSULAMIENTO, manda a llamar el método pero no sabe como se implementa, esta encapsulado
+    //POLIMORFISMO, diferentes formas de hacer lo mismo
+    System.out.println("Kilometros por litro metodo 1 - " + car.calculateConsumption( 300, 0.6f ));
+    System.out.println("Kilometros por litro metodo 2 - " + car.calculateConsumption( 300, 60 ));
+
   }
 }
