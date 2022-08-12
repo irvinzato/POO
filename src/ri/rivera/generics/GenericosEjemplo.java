08d45c5e794267dc383cdf90bd7655e120625681
irvinzato <multizato@hotmail.com>
2022-08-11 22:43:58 -0500

    Empece con uso de genericos, package creado al mismo nivel de interfacespoo

---
+++ src/ri/rivera/generics/GenericosEjemplo.java
@@ -0,0 +1,50 @@
+package ri.rivera.generics;
+import ri.rivera.interfacespoo.modelo.Cliente;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenericosEjemplo {
+  public static void main(String[] args) {
+    //Los genéricos ayudan mucho a evitar errores en especial de casteo
+    List<Cliente> clients = new ArrayList<>();
+    clients.add(new Cliente("Irving", "Rivera"));
+
+    Cliente irving = clients.get(0);
+
+    Cliente[] clientsArr = {new Cliente("Irving", "Rivera"), new Cliente("Angeles", "Lopez"),
+            new Cliente("Jesus", "Rivera"), new Cliente("Jade", "Lopez")};
+
+    Integer[] integersArr = { 1, 2, 3, 4, 5, 6 };
+
+    //Puedo convertir un arreglo de clientes a una lista usando el método de forma genérica y lo mismo con arreglos
+    List<Cliente> clientsList = fromArrayToList(clientsArr);
+    List<Integer> integersList = fromArrayToList(integersArr);
+
+
+    System.out.println("Lista de clientes");
+    clientsList.forEach(System.out::println);
+    System.out.println("Lista de enteros");
+    integersList.forEach(System.out::println);
+
+    System.out.println("Metodo generico con 2 atributos");
+    List<String> stringList = fromArrayToList(new String[]{"Irving", "Jade", "Angeles", "Jesus"}, integersArr);
+    stringList.forEach(System.out::println);
+
+  }
+
+  //Antes el método retornaba "List<Cliente>" y recibía un arreglo de clientes, lo modifiqué de la sig. manera para que acepte cualquier lista
+  //Métodos genericos(Mucho más reutilizables, se aplican a muchos tipos de datos)
+  public static <T> List<T> fromArrayToList(T[] c) {
+    return Arrays.asList(c);  //Método que convierte un arreglo a una lista
+  }
+
+  //Un elemento solo lo itera "g" y el otro lo combierte a lista
+  public static <T, G> List<T> fromArrayToList(T[] t, G[] g) {
+    for(G element: g) {
+      System.out.println(element);
+    }
+    return Arrays.asList(t);
+  }
+
+}
