4511ce4e3990041042e69d653a71f95bfd6a1572
irvinzato <multizato@hotmail.com>
2022-08-12 00:33:21 -0500

    Tranforme la clase Camion a generica, en clase pricipal añadi genericos, ya no es necesario el casteo

--- src/ri/rivera/genericsclass/Camion.java
+++ src/ri/rivera/genericsclass/Camion.java
@@ -4,27 +4,28 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Camion implements Iterable{
-  private List objets;
+//Si no implemento "Iterable" no permite utilizar los "for" de los objetos en la clase principal
+//Una clase genérica también lleva el diamante "<>" para indicar
+public class Camion<T> implements Iterable<T>{
+  private List<T> objets;
   private int maxElements;
 
   public Camion(int maxElements) {
     this.maxElements = maxElements;
-    this.objets = new ArrayList();
+    this.objets = new ArrayList<>();
   }
 
-  public void add(Object object) {
+  public void add(T object) {
     if( this.objets.size() <= maxElements ) {
       this.objets.add(object);
     } else {
       //Sirve para manejar errores, es un objeto de excepción
       throw new RuntimeException("No hay mas espacio en el camión");
     }
-
   }
 
   @Override
-  public Iterator iterator() {
+  public Iterator<T> iterator() {
     return this.objets.iterator();
   }
 }
--- src/ri/rivera/genericsclass/GenericosEjemplo.java
+++ src/ri/rivera/genericsclass/GenericosEjemplo.java
@@ -1,8 +1,9 @@
 package ri.rivera.genericsclass;
 
 public class GenericosEjemplo {
+
   public static void main(String[] args) {
-    Camion truckHorses = new Camion(5);
+    Camion<Animal> truckHorses = new Camion<>(5);
     truckHorses.add(new Animal("Peregrino", "Caballo"));
     truckHorses.add(new Animal("Lenguas", "Caballo"));
     truckHorses.add(new Animal("Glu", "Caballo"));
@@ -11,28 +12,43 @@ public class GenericosEjemplo {
     truckHorses.add(new Animal("Hades", "Caballo"));
 
     System.out.println("Camión de animales");
-    for (Object o : truckHorses) {
-      System.out.println(((Animal) o).getName() + " tipo: " + ((Animal) o).getType());
+    for(Animal a : truckHorses) {
+      System.out.println(a.getName() + " tipo: " + a.getType());
     }
 
-    Camion truckMachines = new Camion(3);
+    Camion<Maquinaria> truckMachines = new Camion(3);
     truckMachines.add(new Maquinaria("Bulldozer"));
     truckMachines.add(new Maquinaria("Grúa Horquilla"));
     truckMachines.add(new Maquinaria("Perforadora"));
 
     System.out.println("Camión de maquinaria");
-    for(Object o: truckMachines) {
-      System.out.println( ((Maquinaria) o).getType() );
+    for(Maquinaria m: truckMachines) {
+      System.out.println( m.getType() );
     }
 
-    Camion truckCar = new Camion(3);
+    Camion<Automovil> truckCar = new Camion(3);
     truckCar.add(new Automovil("Toyota"));
     truckCar.add(new Automovil("Mitsubishi"));
     truckCar.add(new Automovil("Hyundai"));
 
     System.out.println("Camión de autos");
-    for(Object o: truckCar) {
-      System.out.println( ((Automovil) o).getBrand() );
+    for(Automovil a: truckCar) {
+      System.out.println( a.getBrand() );
+    }
+  }
+
+  //Otra manera de hacer el for del método principal, llamando este método
+  public static <T> void toPrintTruck(Camion<T> truck) {
+    for(T a : truck) {
+      if( a instanceof Animal ){
+        System.out.println( ((Animal)a).getName() + " tipo: " + ((Animal)a).getType());
+      }
+      if( a instanceof Maquinaria ){
+        System.out.println( ((Maquinaria)a).getType());
+      }
+      if( a instanceof Automovil ){
+        System.out.println( ((Automovil)a).getBrand());
+      }
     }
   }
 }
