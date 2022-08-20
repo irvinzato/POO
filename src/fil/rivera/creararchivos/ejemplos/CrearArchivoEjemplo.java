a5df4ee43778185d9a260c58105392c4e75852d3
irvinzato <multizato@hotmail.com>
2022-08-19 23:16:48 -0500

    BufferWriter y PrintWriter para crear archivos, mas optimizados

--- src/fil/rivera/creararchivos/ejemplos/CrearArchivoEjemplo.java
+++ src/fil/rivera/creararchivos/ejemplos/CrearArchivoEjemplo.java
@@ -6,9 +6,13 @@ public class CrearArchivoEjemplo {
   public static void main(String[] args) {
     //Muy importante que la ruta donde se quiere guardar el archivo exista
     String nameFile = "D:\\Cursos\\Java\\archivo\\java.txt";
+    String nameFile2 = "D:\\Cursos\\Java\\archivo\\javaBuffer.txt";
+    String nameFile3 = "D:\\Cursos\\Java\\archivo\\javaPrintWriter.txt";
     ArchivoService fileService = new ArchivoService();
 
-    fileService.createFile(nameFile);
+    fileService.createFileWriter(nameFile);
+    fileService.createFileBuffer(nameFile2);
+    fileService.createFilePrintWriter(nameFile3);
 
 
   }
--- src/fil/rivera/creararchivos/ejemplos/servicio/ArchivoService.java
+++ src/fil/rivera/creararchivos/ejemplos/servicio/ArchivoService.java
@@ -1,12 +1,10 @@
 package fil.rivera.creararchivos.ejemplos.servicio;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class ArchivoService {
 
-  public void createFile(String nameFile) {
+  public void createFileWriter(String nameFile) { //Para trabajar con pocas líneas de texto
     File file = new File(nameFile);
     try {
       FileWriter writer = new FileWriter(file); //El segundo parámetro en "true" aria que se agregue contenido cada que se ejecuta
@@ -14,7 +12,38 @@ public class ArchivoService {
               .append("Siempre adelante !\n")
               .append("Vamos a darle con todo");
       writer.close(); //Importante el "close" para cerrar el recurso y se pueda añadir lo de arriba
-      System.out.println("El archivo se creo con éxito !");
+      System.out.println("El archivo se creo con éxito ! usando FileWriter");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void createFileBuffer(String nameFile) {
+    File file = new File(nameFile);
+    try {
+      FileWriter writer = new FileWriter(file);  //Se encarga de crear el archivo
+      BufferedWriter buffer = new BufferedWriter(writer); //Es mas optimizado que "FileWriter" para escribir cantidades
+      buffer.append("Añadiendo texto al archivo que se va a crear...")
+              .append("Siempre adelante !\n")
+              .append("Vamos a darle con todo");
+      buffer.close();
+      System.out.println("El archivo se creo con éxito ! usando BufferedWriter");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void createFilePrintWriter(String nameFile) {
+    File file = new File(nameFile);
+    try {
+      FileWriter writer = new FileWriter(file);  //Se encarga de crear el archivo
+      PrintWriter print = new PrintWriter(writer); //Más optimizado que "FileWriter", permite usar métodos como el "printline" escribir en el archivo como si fuera una consola
+      //No es encadenado
+      print.println("Añadiendo texto al archivo que se va a crear...");
+      print.println("Siempre adelante !");
+      print.printf("Vamos a darle con todo %s", "Irving");
+      print.close();
+      System.out.println("El archivo se creo con éxito ! usando PrintWriter");
     } catch (IOException e) {
       e.printStackTrace();
     }
